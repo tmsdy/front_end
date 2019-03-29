e539fdf06432f3064c1980b9504b66a909cc86f9
fei_han <fei_han@fei-handeMac-mini.local>
2019-03-29 21:15:12 +0800

    19.3.29

--- internet/工具/charles.js
+++ internet/工具/charles.js
@@ -1,11 +1,12 @@
 /*
 
-1.抓包之Charles For Mac 4.0+破解版：https://www.jianshu.com/p/1c1023036a75
+1.抓包之Charles For Mac 4.0+破解版：https://www.jianshu.com/p/1c1023036a75，没注册的charles,30分钟就会关闭一次。
 
 2.mac os proxy勾上这样电脑上走的请求全都可以被抓到
 
-3.https乱码问题：Help -> SSL Proxying -> Install Charles Root Certificate，装ca证书
-    不过失败了
+3.抓https：Help -> SSL Proxying -> Install Charles Root Certificate，装ca证书
+    要配好*:433的端口
+手机上用Help -> SSL Proxying -> 。。。。
 
 4.过滤网络请求：Proxy -> Recording Setting -> include添加白名单
 
@@ -13,6 +14,7 @@
 
 6.多次请求：sequence看到请求，右键有repeat，可以再次请求，repeat advanced可以多次请求
 
+7.找到符合某条件的请求：右键左边的域名，选择find In
 
 
 */
---
+++ 移动端/H5/抽奖/做的事.js
@@ -0,0 +1,10 @@
+/*
+
+1.把图片button等图片的地方用css实现
+
+2.跑马灯的地方用了transform: translateZ(0)来优化渲染性能
+
+3.
+
+
+*/
