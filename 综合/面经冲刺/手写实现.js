0985a3725e45713619ee3182d0e6e14f6f0d134a
韩飞 <13162352330@163.com>
2019-10-03 15:53:17 +0800

    19.10.3

---
+++ 综合/面经冲刺/手写实现.js
@@ -0,0 +1,25 @@
+/*
+
+1.手写实现以下事件委托函数
+function delegate(parent, selector, handle) {}
+
+2.手写实现inherit函数
+
+3.手写实现throttle函数,手写 debounce 函数
+
+4.需要设计一个拖拽的dialog，怎么实现？手写代码
+
+5.手写代码实现一下Array.prototype.trim 这个函数，并写个测试用例跑给我看下
+
+6.实现一个repeat函数，主要是闭包的应用
+
+7.手写继承（ES5、ES6）
+
+8. 实现 sum 函数
+sum(1) == 1;
+sum(1)(2) == 3;
+sum(1)(2, 3, 4)(5) == 15;
+
+9.手写观察者模式
+
+*/
