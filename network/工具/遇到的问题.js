c0b9e89458f9e5dff3e00bb87a9f3355d18e2356
韩飞 <13162352330@163.com>
2019-10-21 18:11:43 +0800

    19.10.21

--- network/工具/遇到的问题.js
+++ network/工具/遇到的问题.js
@@ -1,6 +1,6 @@
 /*
 1.抓https包乱码
-
+https://www.jianshu.com/p/2712e53bb761
 
 
 */
--- react/1.learn/1.todolist/src/TodoItem.js
+++ react/1.learn/1.todolist/src/TodoItem.js
@@ -1,37 +1,37 @@
-import React, { Component,Fragment } from 'react'
+import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
 
 export class TodoItem extends Component {
-  constructor(props){
-    super(props)
-    this.deleteItem = this.deleteItem.bind(this)
-  }
-  deleteItem(){
-    let {item,deleteItem} = this.props
-    deleteItem(item.id)
-  }
-  // 父组件render子组件都render会有性能损失，在这里减少一些不必要的render
-  shouldComponentUpdate(nextProps,nextState){
-    if(nextProps.item.content !== this.props.content){
-      return true
+    constructor(props) {
+        super(props)
+        this.deleteItem = this.deleteItem.bind(this)
     }
-    return false
-  }
-  render() {
-    let {item} = this.props
-    return (
-      <Fragment>
-        <li >{item.content}<span className="item_delete" onClick={this.deleteItem}>删除</span></li>
-      </Fragment>
-    )
-  }
+    deleteItem() {
+        let { item, deleteItem } = this.props
+        deleteItem(item.id)
+    }
+    // 父组件render子组件都render会有性能损失，在这里减少一些不必要的render
+    shouldComponentUpdate(nextProps, nextState) {
+        if (nextProps.item.content !== this.props.content) {
+            return true
+        }
+        return false
+    }
+    render() {
+        let { item } = this.props
+        return (
+            <Fragment>
+                <li >{item.content}<span className="item_delete" onClick={this.deleteItem}>删除</span></li>
+            </Fragment>
+        )
+    }
 }
 TodoItem.propTypes = { //传值类型校验
-  item: PropTypes.object.isRequired,
-  deleteItem: PropTypes.func
+    item: PropTypes.object.isRequired,
+    deleteItem: PropTypes.func
 }
 TodoItem.defaultProps = {
-  item: {}
+    item: {}
 }
 
 export default TodoItem
