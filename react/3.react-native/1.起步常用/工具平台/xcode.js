2617be613766dc11929f9962b6dee08b9ea05e79
韩飞 <13162352330@163.com>
2019-11-12 15:22:23 +0800

    19.11.12

--- react/3.react-native/1.起步常用/工具平台/xcode.js
+++ react/3.react-native/1.起步常用/工具平台/xcode.js
@@ -1,9 +1,16 @@
 /*
+xcode各种版本下载：https://developer.apple.com/download/more/
 
 1.切换模拟器：https://www.jianshu.com/p/bb1ba50700bc
 
 2.ios真机调试：https://www.jianshu.com/p/8c99cb119bb3
 调试卡在启动图原因及解决：https://www.jianshu.com/p/411b4d6c82a6
+
+3.xcode11.2拖动生成的路径不对。
+
+4.卸掉xcode：https://www.jianshu.com/p/c93d5b64aa58
+卸载出现的问题xcrun: error: active developer path
+解决：sudo xcode-select --reset
 
 
 
---
+++ 移动端/考点总结/适配和响应式/demo/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="app">
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'app',
+}
+</script>
+
+<style lang="less">
+/* @rem
+当页面宽度为375px时候，设置375px为正确的大小。
+保证 375 * @rem = 375，iphone6的时候正好window.devicePixelRatio = 2
+-> @rem = 1px -> 当前页面宽为375时，除以25得到15px赋值给html的fontSize -> 1rem = 15px
+-> @rem = 1/15rem = 1px
+*/
+// @rem: 1/15rem;
+// #app {
+//     width: 360 * @rem;
+//     // width: 360px;
+//     // width: 310px;
+//     height: 60px;
+//     background: pink;
+// }
+
+/*
+rem 当页面宽度为375px时候，目标设置3.75rem为正确的大小。那就把html的fontSize搞到100px
+document.documentElement.style.fontSize = html.clientWidth / 3.75 + 'px';
+*/
+#app {
+    width: 3.6rem;
+    height: 60px;
+    background: pink;
+}
+</style>
