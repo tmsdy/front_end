77651a780c9ec1a96a11e8ea492bc361b9981754
韩飞 <13162352330@163.com>
2019-12-10 14:29:29 +0800

    19.12.10

---
+++ network/1.性能优化/3.运行时&渲染/3.vue&react优化/react fiber.js
@@ -0,0 +1,9 @@
+/*
+
+
+
+参考：
+https://zhuanlan.zhihu.com/p/35578843
+https://github.com/MuYunyun/blog/blob/master/React/%E4%BD%A0%E4%B8%8D%E7%9F%A5%E9%81%93%E7%9A%84requestIdleCallback.md
+
+*/
--- react/框架对比.js
+++ react/框架对比.js
@@ -13,6 +13,9 @@ Vue:
  1) 用Proxy代替Object.defineProperty，解决了不能改数组索引的问题还有提高了响应数据变化的速度。
  2）优化传统虚拟DOM diff的性能瓶颈，采用block tree的策略，静态节点标记。快5-6倍，1w2个dom diff只需5ms
  3）
-
+React:
+ 1) fiber时间分片、调度。在eventloop一个tick中js执行时间过长会导致渲染延迟，页面就变卡。
+于是需要一个时间调度器。浏览器刚好实现一个requestIdleCallback。requestIdleCallback根据参数的不同，可以在限度时间内安排一定量的JS任务，从而不影响视图渲染/事件回调; 也可以强制在浏览器不断更新视图的瞎忙中，强制中断这个行为，立即安插进我们React JS逻辑。
+我们在requestIdleCallback的回调中加入一个WorkLoop的方法，它每接触一个fiber时，就判定一下当前时间，看否有空闲时间让它进行beginWork操作（相当于刚才的第一个阶段，设置dom, instance, willXXX），没有就把它放进列队中。把控制权让渡给视图渲染。下次requestIdleCallback唤起时，从列队将刚才那个fiber取出来，执行beginWork。
 
 */
