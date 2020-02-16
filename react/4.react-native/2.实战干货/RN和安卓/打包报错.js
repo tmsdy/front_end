7c9b992607589149df722f53db1f8850da10644a
韩飞 <13162352330@163.com>
2020-02-16 16:09:46 +0800

    20.2.16

--- react/4.react-native/2.实战干货/RN和安卓/打包报错.js
+++ react/4.react-native/2.实战干货/RN和安卓/打包报错.js
@@ -1,13 +1,14 @@
 /*
 
 1.Task :app:mergeReleaseStagingResources FAILED
-
-2.在app的gradle配置文件中，android下加上下面一段代码：
+在app的gradle配置文件中，android下加上下面一段代码：
 aaptOptions {
     cruncherEnabled = false //禁止AS对png图片进行校验
 }
 最后是吧对应报错目标的png图片删了解决的。
 
+2.com.android.builder.dexing.DexArchiveMergerException: Error while merging dex archives
+原因：加了react-native-camera里面的包起冲突了
 
 
 
