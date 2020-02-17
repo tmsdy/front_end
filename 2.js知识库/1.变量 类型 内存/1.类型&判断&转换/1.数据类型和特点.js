9e735bdd13cd40d06b54577f0f571985a09d2fc1
韩飞 <13162352330@163.com>
2020-02-17 21:01:45 +0800

    20.2.17

--- 2.js知识库/1.变量 类型 内存/1.类型&判断&转换/1.数据类型和特点.js
+++ 2.js知识库/1.变量 类型 内存/1.类型&判断&转换/1.数据类型和特点.js
@@ -1,10 +1,10 @@
 /*
 
-基本类型值：undefined,null,boolean,number,string
+* 基本数据类型：undefined,null,boolean,number,string，Symbol(ES6)
     - 按值访问，可以直接改变目标值。
     - 存在栈中
 
-引用类型值：Object Symbol(ES6)
+* 引用数据类型：Object
     - 按引用访问，js不允许直接操作内存地址，可以操作对象的引用
     - 对象存在栈中，它的引用指向堆中的内存地址。以key-value存在
     {
---
+++ 2.js知识库/1.变量 类型 内存/1.类型&判断&转换/test.js
@@ -0,0 +1,6 @@
+function Parent() {
+    this.name = 'parent'
+}
+let parent = new Parent()
+
+console.log(parent instanceof Object) // true
