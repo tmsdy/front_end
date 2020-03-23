2598b3ca15409079d55e3f3ac6950e17dae6ba28
韩飞 <hanfei19961016@gmail.com>
2020-03-23 09:58:20 +0800

    20.3.23

--- 2.js知识库/4.数组 Date 正则等/3.正则/实际应用/1.常见正则.js
+++ 2.js知识库/4.数组 Date 正则等/3.正则/实际应用/1.常见正则.js
@@ -1,10 +1,3 @@
-// 1.常见
-var re = {
-    chinese: /[\u4e00-\u9fa5]/, //中文
-    qq: /^[1-9]\d{4,11}$/,
-    email: /^\w+@[a-z0-9]+(\.[a-z]+){1,3}$/, //邮箱
-    id_card: /[1-9]\d{14}|[1-9]\d{17}|[1-9]\d{16}x/,//身份证
-}
 
 // 2.千分位加逗号
 var num1 = '12000000.11';
@@ -12,13 +5,13 @@ var num1 = '12000000.11';
 console.log(commafy(num1))
 
 function commafy(num) { //有小数点的
-    return num && num.toString().replace(/(\d)(?=(\d{3})+\.)/g, function ($1, $2) {
-        return $2 + ',';
-    });
+  return num && num.toString().replace(/(\d)(?=(\d{3})+\.)/g, function ($1, $2) {
+    return $2 + ',';
+  });
 }
 
 function commafy_2(num) { //没有小数点的
-    return num && num.toString().replace(/(?=(?!\b)(\d{3})+$)/g, ',');
+  return num && num.toString().replace(/(?=(?!\b)(\d{3})+$)/g, ',');
 }
 
 // 3.从文件路径中匹配文件名
