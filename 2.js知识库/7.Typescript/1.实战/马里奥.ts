e06b6d5081d3cec47c1034221b325c0089295079
韩飞 <hanfei19961016@gmail.com>
2020-04-11 01:09:20 +0800

    20.4.11

---
+++ 2.js知识库/7.Typescript/1.实战/马里奥.ts
@@ -0,0 +1,13 @@
+// 1.怎样给解构值赋类型
+var addListener = ({ urls, cancel }: { urls: string[], cancel?: Boolean }) => {
+
+}
+
+// 2.自己建一个index.d.ts引入来自定义类型
+declare namespace chrome.runtime {
+  function sendMessageAsync({ type }: { type: string }): Promise<any>
+}
+
+interface Element extends Node, Animatable, ChildNode, InnerHTML, NonDocumentTypeChildNode, ParentNode, Slotable {
+  [x: string]: any; // 解决dom上属性的问题
+}
--- 5.react/1.learn/3.todolist-finally/src/page/index.js
+++ 5.react/1.learn/3.todolist-finally/src/page/index.js
@@ -3,11 +3,11 @@ import TodoList from './TodoList'
 import Counter from './Counter'
 
 export default (props) => {
-    return (
-        <div>
-            <TodoList></TodoList>
-            <br></br>
-            <Counter></Counter>
-        </div>
-    )
+  return (
+    <div>
+      <TodoList></TodoList>
+      <br></br>
+      <Counter></Counter>
+    </div>
+  )
 }
