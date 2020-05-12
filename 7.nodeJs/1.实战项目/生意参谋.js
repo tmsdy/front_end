bea81d3677af358ed04a85bf6156946c9af838a4
韩飞 <hanfei19961016@gmail.com>
2020-05-12 13:39:22 +0800

    20.5.12

---
+++ 7.nodeJs/1.实战项目/生意参谋.js
@@ -0,0 +1,6 @@
+/*
+* 1.市场搜索词排行：https://sycm.taobao.com/mc/mq/search_rank
+在vendors.js中有解密的key
+
+
+*/
--- 7.nodeJs/online/1.nginx/各种问题.js
+++ 7.nodeJs/online/1.nginx/各种问题.js
@@ -1,8 +1,9 @@
 /*
 1. nginx: [error] invalid PID number "" in "/run/nginx.pid"
 执行 nginx -t 是OK的，然而在执行 nginx -s reload 的时候报错
-解决：先nginx -c /etc/nginx/nginx.conf，nginx.conf文件的路径可以从nginx -t的返回中找到。
-再nginx -s reload
+解决：
+sudo nginx -c /usr/local/etc/nginx/nginx.conf
+sudo nginx -s reload
 
 2.nginx: [alert] kill(21725, 1) failed (1: Operation not permitted)
 解决：nginx 缺少启动权限，sudo nginx -s reload
